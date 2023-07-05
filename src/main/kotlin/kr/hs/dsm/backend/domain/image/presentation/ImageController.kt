5e5b79518a34e2be99d9bda49cbfdb6f3e9701f1
rlaisqls <rlaisqls@gmail.com>
2023-07-05 09:04:33 +0900

    feat: 이미지 고치기

--- src/main/kotlin/kr/hs/dsm/backend/domain/image/presentation/ImageController.kt
+++ src/main/kotlin/kr/hs/dsm/backend/domain/image/presentation/ImageController.kt
@@ -6,12 +6,16 @@ import com.amazonaws.services.s3.model.CannedAccessControlList
 import com.amazonaws.services.s3.model.ObjectMetadata
 import com.amazonaws.services.s3.model.PutObjectRequest
 import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
+import java.util.UUID
 import kr.hs.dsm.backend.domain.image.config.AwsS3Properties
 import kr.hs.dsm.backend.global.error.InternalServerError
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
 
 @RestController
 class ImageController(
@@ -19,13 +23,27 @@ class ImageController(
     private val amazonS3Client: AmazonS3Client
 ) {
 
-    @PostMapping("/image")
-    fun uploadImage(@RequestPart file: File): ImageUrlResponse {
+    @PostMapping("/image", consumes = [
+        MediaType.MULTIPART_FORM_DATA_VALUE,
+        MediaType.IMAGE_PNG_VALUE,
+        MediaType.IMAGE_JPEG_VALUE
+    ])
+    fun uploadImage(@RequestPart("file") multiPartFile: MultipartFile): ImageUrlResponse {
+        val file = multiPartFile.toFile()
         inputS3(file, file.name)
         return ImageUrlResponse(
             fileUrl = getResourceUrl(file.name)
         )
     }
+
+    fun MultipartFile.toFile() =
+        File("${UUID.randomUUID()}||$originalFilename").let {
+            FileOutputStream(it).run {
+                this.write(bytes)
+                this.close()
+            }
+            it
+        }
 
     private fun inputS3(file: File, fileName: String) {
         try {
